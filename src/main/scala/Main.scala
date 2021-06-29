9d54876bc4d0266b3bc2d7f477d74a9455a09a68
Luyu Cheng <chengluyu@live.cn>
2021-06-30 03:48:40 +0800

    feat: add `toString` to `Term` classes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -63,23 +63,29 @@ object Type {
 
 trait Term {
   def getType(context: Context): Type
+  override def toString(): String
 }
 
 object Term {
   final case class True() extends Term {
     def getType(context: Context): Type = new Type.Bool
+    override def toString: String = "true"
   }
   final case class False() extends Term {
     def getType(context: Context): Type = new Type.Bool
+    override def toString: String = "false"
   }
   final case class Int(value: scala.Int) extends Term {
     def getType(context: Context): Type = new Type.Int
+    override def toString: String = value.toString()
   }
   final case class Record(entries: HashMap[String, Term]) extends Term {
     def getType(context: Context): Type =
       new Type.Record(
         entries map (entry => (entry._1, entry._2.getType(context)))
       )
+    override def toString: String = "{ " + (for ((name, value) <- entries)
+      yield name + ": " + value.toString()) + " }"
   }
   final case class If(test: Term, consequent: Term, alternate: Term)
       extends Term {
@@ -98,6 +104,9 @@ object Term {
       }
       case _ => throw new MyException("test must evaluate to a boolean")
     }
+    override def toString: String =
+      "if " + test.toString + " then " + consequent.toString + " else " +
+        alternate.toString
   }
   final case class Project(target: Term, field: String) extends Term {
     def getType(context: Context): Type = target.getType(context) match {
@@ -105,14 +114,21 @@ object Term {
         entries.getOrElse(field, throw new MyException("invalid field"))
       case _ => throw new MyException("only record types are projectable")
     }
+    override def toString: String = target match {
+      case target @ If(_, _, _) => "(" + target.toString + ")" + "." + field
+      case _                    => target.toString + "." + field
+    }
   }
   final case class Variable(name: String) extends Term {
     def getType(context: Context): Type = context.getType(name)
+    override def toString: String = name
   }
   final case class Abstract(parameter: String, parameterType: Type, body: Term)
       extends Term {
     def getType(context: Context): Type =
       Type.Function(parameterType, body.getType(context))
+    override def toString: String =
+      "(" + parameter + ": " + parameterType.display + ") => " + body.toString
   }
   final case class Apply(callee: Term, argument: Term) extends Term {
     def getType(context: Context): Type = callee.getType(context) match {
@@ -126,6 +142,10 @@ object Term {
       }
       case _ => throw new MyException("callee is not a function")
     }
+    override def toString: String = (callee match {
+      case callee @ If(_, _, _) => "(" + callee.toString + ")"
+      case _                    => callee.toString
+    }) + "(" + argument.toString + ")"
   }
 }
 
