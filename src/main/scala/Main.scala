0c3728b3efbc4ab013d3e60b612c30b25f5d0aa5
Luyu Cheng <chengluyu@live.cn>
2021-06-30 14:18:33 +0800

    feat: add type evaluation to the REPL

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,20 +4,28 @@ import fastparse.Parsed.Success
 import fastparse.Parsed.Failure
 
 object Main extends App {
+  val context = new Context()
+
   def repl: Unit = while (true) {
     val command = readLine("> ")
     command match {
-      case "quit"  => return
-      case "exit"  => return
-      case command => Parser.parse(command) match {
-        case Success(value, _) => {
-          println(s"Re-serialized term: $value")
+      case "quit" => return
+      case "exit" => return
+      case command =>
+        Parser.parse(command) match {
+          case Success(term, _) => {
+            try {
+              val termType = term.getType(context)
+              println(s"$term :: $termType")
+            } catch {
+              case e: MyException => println(s"Type error: ${e.getMessage}")
+            }
+          }
+          case Failure(_, index, extra) => {
+            val message = extra.trace().longAggregateMsg
+            println(s"Syntax error at $index: $message")
+          }
         }
-        case Failure(_, index, extra) => {
-          val message = extra.trace().longAggregateMsg
-          println(s"Syntax error at $index: $message")
-        }
-      }
     }
   }
 
