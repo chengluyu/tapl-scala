dd3ceea0b2fad4fc61b81df03c121fcb87fc5dd2
Luyu Cheng <chengluyu@live.cn>
2021-07-01 01:00:43 +0800

    fix: resolve ambiguities in the parser

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -2,49 +2,48 @@ import fastparse._, SingleLineWhitespace._
 import scala.collection.immutable.HashMap
 
 object Parser {
-  def parse(x: String) = fastparse.parse(x, TermParser.term(_))
+  def parse(x: String) = fastparse.parse(x, TermParser.root(_))
 
-  private def ident[_: P]: P[String] = CharIn("a-zA-Z").rep(1).!
+  private def ident[_: P]: P[String] = CharIn("a-zA-Z").repX(1).!
 
   private object TermParser {
-    def term[_: P]: P[Term] = P(compound ~ End)
-    def compound[_: P]: P[Term] = P(
-      (atom ~ (apply | project).rep).map({ case (a, b) =>
+    def root[_: P]: P[Term] = P(term ~ End)
+    def term[_: P]: P[Term] = P(complex | simpleWithTails)
+    def simple[_: P]: P[Term] = P(yea | nay | int | variable | record | paren)
+    def simpleWithTails[_: P]: P[Term] = P(
+      (simple ~/ tail.rep).map({ case (a, b) =>
         b.foldLeft(a)((target, postfix) =>
           postfix match {
-            case argument: Term => new Term.Apply(target, argument)
-            case name: String   => new Term.Project(target, name)
+            case Left(argument) => new Term.Apply(target, argument)
+            case Right(name)    => new Term.Project(target, name)
           }
         )
       })
     )
-    def apply[_: P]: P[Term] = P("(" ~/ compound ~ ")")
-    def project[_: P]: P[String] = P("." ~/ ident)
-    def atom[_: P]: P[Term] = P(
-      yea | nay | int | paren | record | condition | abstraction | variable
-    )
-    def paren[_: P]: P[Term] = P("(" ~ compound ~ ")")
+    def complex[_: P]: P[Term] = P(condition | abstraction)
+    def applyTail[_: P] = P(("(" ~/ term.map(Left(_)) ~ ")"))
+    def projectTail[_: P] = P("." ~/ ident.map(Right(_)))
+    def tail[_: P]: P[Either[Term, String]] = P(applyTail | projectTail)
+    def paren[_: P]: P[Term] = P("(" ~ term ~ ")")
     def yea[_: P]: P[Term.True] = P("true").map(_ => new Term.True())
     def nay[_: P]: P[Term.False] = P("false").map(_ => new Term.False())
     def int[_: P]: P[Term.Int] = P(
-      CharIn("0-9").rep(1).!.map(x => new Term.Int(x.toInt))
+      CharIn("0-9").repX(1).!.map(x => new Term.Int(x.toInt))
     )
     def condition[_: P]: P[Term.If] = P(
-      ("if" ~/ compound ~ "then" ~ compound ~ "else" ~ compound).map({
-        case (a, b, c) =>
-          new Term.If(a, b, c)
+      ("if" ~/ term ~/ "then" ~/ term ~/ "else" ~/ term).map({ case (a, b, c) =>
+        new Term.If(a, b, c)
       })
     )
     def abstraction[_: P]: P[Term.Abstract] = P(
-      ("(" ~ ident ~ ":" ~ TypeParser.termType ~ ")" ~ "=>" ~ compound).map({
+      ("(" ~ ident ~ ":" ~ TypeParser.termType ~ ")" ~ "=>" ~ term).map({
         case (a, b, c) =>
           new Term.Abstract(a, b, c)
       })
     )
-    def variable[_: P]: P[Term.Variable] = P(ident.map(new Term.Variable(_)))
-
+    def variable[_: P]: P[Term.Variable] = P(ident.map(Term.Variable))
     def record[_: P]: P[Term.Record] = P(
-      "{" ~/ (ident ~/ ":" ~/ compound)
+      "{" ~/ (ident ~/ ":" ~/ term)
         .rep(sep = ",")
         .map(HashMap.from)
         .map(Term.Record) ~ "}"
@@ -61,8 +60,9 @@ object Parser {
     def top[_: P]: P[Type] = P("Any").map(_ => new Type.Top())
     def paren[_: P]: P[Type] = P("(" ~/ termType ~ ")")
     def record[_: P]: P[Type] = P(
-      "{" ~ entry.rep(sep = ",").map(HashMap.from).map(Type.Record) ~ "}"
+      "{" ~/ entry.rep(sep = ",").map(HashMap.from).map(Type.Record) ~ "}"
     )
     def entry[_: P]: P[(String, Type)] = P(ident ~/ ":" ~ termType)
+
   }
 }
