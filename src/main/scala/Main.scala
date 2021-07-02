de38a63a6df0f634a63528757eb1ac6342eb3017
Luyu Cheng <chengluyu@live.cn>
2021-07-02 15:00:38 +0800

    refactor: rewrite REPL in a more extensible fashion

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,32 +2,67 @@
 import scala.io.StdIn.readLine
 import fastparse.Parsed.Success
 import fastparse.Parsed.Failure
+import scala.util.matching.Regex
 
 object Main extends App {
   val context = new Context()
+  val commandPattern = """^:(\w+)(?: (.*))?$""".r("command", "body")
 
-  def repl: Unit = {
-    val command = readLine("> ")
+  def perform(command: String, body: String): ReplResult =
     command match {
-      case "quit" =>
-      case "exit" =>
-      case command =>
-        Parser.parse(command) match {
+      case "type" =>
+        Parser.parse(body) match {
           case Success(term, _) =>
             try {
               val termType = term.getType(context)
-              println(s"$term :: $termType")
+              ReplResult.Success(s"$term :: $termType")
             } catch {
-              case e: MyException => println(s"Type error: ${e.getMessage}")
+              case e: MyException =>
+                ReplResult.Failture(s"Type error: ${e.getMessage}")
             }
           case Failure(_, index, extra) =>
             val message = extra.trace().longAggregateMsg
-            println(s"Syntax error at $index: $message")
+            ReplResult.Failture(s"Syntax error at $index: $message")
         }
+      case "quit" => ReplResult.Termination()
+      case "exit" => ReplResult.Termination()
+      case "help" => ReplResult.Success("""Available commands:
+        | - type <expr>: show type of expr (the default behavior)
+        | - quit       : exit the program
+        | - exit       : exit the program
+        | - help       : show this message""".stripMargin)
+    }
+
+  def repl: Unit =
+    (readLine("> ") match {
+      case line if line.startsWith(":") =>
+        commandPattern findFirstMatchIn line match {
+          case Some(value) =>
+            perform(value.group("command"), value.group("body"))
+          case None => ReplResult.Failture(s"unknown command")
+        }
+      case code => perform("type", code)
+    }) match {
+      case ReplResult.Termination() =>
+        println("Bye.")
+      case ReplResult.Success(result) =>
+        println(result)
+        repl
+      case ReplResult.Failture(message) =>
+        println(message)
         repl
     }
-  }
 
   println("Welcome to TAPL Scala!")
   repl
+}
+
+trait ReplResult {
+  override def toString: String
+}
+
+object ReplResult {
+  case class Termination() extends ReplResult
+  case class Success(result: String) extends ReplResult
+  case class Failture(message: String) extends ReplResult
 }
